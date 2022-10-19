0633f5137f1b0c7f1b327ec482935396bab23e43
fpistagna <fabrizio.pistagna@ingv.it>
2022-10-19 14:55:27 +0200

    General fixes on initdb compose recipe

--- scripts/initdb.sh
+++ scripts/initdb.sh
@@ -16,20 +16,22 @@ psql -h ${DB_HOST} -U ${DB_USER} < /sql_create/create_tsdsystem_role.sql
 echo "END create_tsdsystem_role"
 
 # psql -h ${DB_HOST} -U ${DB_USER} < /sql_create/create_tsdsystem_db.sql
+
+echo "Executing scripts from folder sql_tsd"
 echo "RUN init.sql"
 psql -h ${DB_HOST} -U ${DB_USER} < /sql_tsd/init.sql
 echo "END init.sql"
 
 echo "RUN tsd_main.sql"
-psql -h ${DB_HOST} -U ${DB_USER} < /sql_tsd/tsd_main.sql
+psql -h ${DB_HOST} -U ${DB_USER} -d ${TSD_DB} < /sql_tsd/tsd_main.sql
 echo "END tsd_main.sql"
 
 echo "RUN tsd_pnet.sql"
-psql -h ${DB_HOST} -U ${DB_USER} < /sql_tsd/tsd_pnet.sql
+psql -h ${DB_HOST} -U ${DB_USER} -d ${TSD_DB} < /sql_tsd/tsd_pnet.sql
 echo "END tsd_pnet.sql"
 
 echo "RUN tsd_users.sql"
-psql -h ${DB_HOST} -U ${DB_USER} < /sql_tsd/tsd_users.sql
+psql -h ${DB_HOST} -U ${DB_USER} -d ${TSD_DB} < /sql_tsd/tsd_users.sql
 echo "END tsd_users.sql"
 
 echo "RUN public.sql"
